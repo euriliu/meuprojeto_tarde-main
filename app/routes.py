2c28d1b8f24c4aaad294a1154b115336d4ea70e9
Eduardo Vinicius <edu.natal2011@gmail.com>
2023-08-15 17:06:47 -0300

    15/08/2023-Cadastro_usuario

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, db
 from flask import render_template,url_for,flash,redirect,request
-from app.forms import Contato
-from app.models import ContatoModels
+from app.forms import Contato, Cadastro
+from app.models import ContatoModels, CadastroModels
 import time
 
 @app.route('/')
@@ -14,7 +14,6 @@ def sobre():
 
 @app.route('/contato', methods =['GET', 'POST'])
 def contato():
-    dados_formulario = None
     formulario = Contato()
     if formulario.validate_on_submit():
         flash('mensagem enviada com sucesso!!')
@@ -28,14 +27,33 @@ def contato():
         db.session.commit()
 
 
-    return render_template('contato.html', title = 'contato', formulario = formulario, dados_formulario = dados_formulario)
+    return render_template('contato.html', title = 'contato', formulario = formulario)
 
 @app.route('/projetos')
 def projeto():
     return render_template('projeto.html', title ='Projetos')
 
 
-@app.route('/teste')
-def teste():
-    return render_template('teste.html', title = 'Teste')
+@app.route('/cadastro', methods =['GET', 'POST'])
+def cadastro():
+    cadastro=Cadastro()
+    if cadastro.validate_on_submit():
+        flash('Cadastrado com sucesso!!')
+        nome = cadastro.nome.data
+        email = cadastro.email.data
+        senha = cadastro.senha.data
+        contato = cadastro.contato.data
+        novo_cadastro = CadastroModels(nome = nome, email = email, senha = senha, contato = contato)
+        db.session.add(novo_cadastro)
+        db.session.commit()
+
+
+    return render_template('cadastro.html', title = 'Cadastro', cadastro = cadastro)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title ='Login')
+
+
 
